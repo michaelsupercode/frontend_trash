e17a388e72602568a8b6466d1abb4c1c4b127da1
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-04-28 08:14:39 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,1 @@
-export const apiBaseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000"
+export const apiBaseUrl = process.env.REACT_APP_BACKEND_URL || "https://trashback.onrender.com/"
