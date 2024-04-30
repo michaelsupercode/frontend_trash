35e7d39a6c00924e67a89d2c89e087c0d8228bb1
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-04-30 10:24:49 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,1 @@
-export const apiBaseUrl = process.env.REACT_APP_BACKEND_URL || "https://ntt-back-s6oj.onrender.com/"
+export const apiBaseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000"
