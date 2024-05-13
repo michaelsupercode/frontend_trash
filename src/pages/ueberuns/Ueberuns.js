73991f4faad42b15b9607c9bb874a71183d5261c
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-05-13 15:59:46 +0200

    Update Ueberuns.js

--- src/pages/ueberuns/Ueberuns.js
+++ src/pages/ueberuns/Ueberuns.js
@@ -23,44 +23,7 @@ const Ueberuns = () => {
       </section>
       <h2>NothingToTrash Team</h2>
       <section className="card">
-        <div className="player">
-          <img src="https://www.pokedexia.net/images/3/34/PGL-007.png" alt="" />
-          <h2>Annabelle Backend Developerin</h2>
-          <p>Git-Queen</p>
-          <p>Heroku-Schildmaid</p>
-          <p>Orga-Herrin</p>
-          <p>Backend halt</p>
-        </div>
-        <div className="player ">
-          <img
-            src="https://www.pokewiki.de/images/thumb/4/48/Sugimori_006m2.png/250px-Sugimori_006m2.png"
-            alt=""
-          />
-          <h2>Akhim Frontend Developer</h2>
-          <p>MediaQuerie-Master</p>
-          <p>Styling</p>
-          <p>Überuns</p>
-          <p>Frontend halt</p>
-        </div>
-        <div className="player">
-          <img
-            src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png"
-            alt=""
-          />
-          <h2>Max Frontend Developer</h2>
-          <p>Chef vom Frontend</p>
-          <p>Schnittstelle Frontend/Backend</p>
-          <p>Ordnerstruktur</p>
-          <p>Frontend halt</p>
-        </div>
-        <div className="player">
-          <img src="https://www.pokedexia.net/images/9/9c/PGL-202.png" alt="" />
-          <h2>Semir Backend Developer</h2>
-          <p>Datenbank-Chef</p>
-          <p>Schnittstelle Frontend/Backend</p>
-          <p>Testing</p>
-          <p>Backend halt</p>
-        </div>
+       
       </section>
       <Footer />
       <FooterEnd />
